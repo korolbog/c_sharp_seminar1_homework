13c9fd0efcbf3b4d45c610f1aa3efb9818fd9372
BogdanKorolev <bkorolevjob@gmail.com>
2022-07-12 22:00:31 +0300

    Task8 code written

---
+++ Sem1_HomeworkTask8/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 8: Напишите программу,
+которая на вход принимает число (N),
+а на выходе показывает все чётные числа от 1 до N:
+5 -> 2, 4
+*/
+Console.Write("Введите ваше число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N];
+int index = 0;
+string result = "";
+while (index < N)
+{
+    array[index] = index + 1;
+    if (array[index] % 2 == 0)
+    {
+       result += array[index].ToString() + ", ";
+    } 
+    index++;
+}
+Console.WriteLine(result.Remove(result.Length-2, 2));
+Console.ReadLine();
