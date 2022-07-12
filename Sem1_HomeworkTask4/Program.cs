4793321d5ce156587f203ef8a5b5cead6b0322f7
BogdanKorolev <bkorolevjob@gmail.com>
2022-07-12 19:20:11 +0300

    Task4.0 code written

---
+++ Sem1_HomeworkTask4/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 4: Напишите программу,
+которая принимает на вход три числа
+и выдаёт максимальное из этих чисел: 2, 3, 7 -> 7 */
+
+int numberA = 2;
+int numberB = 3;
+int numberC = 7;
+int max = numberA;
+if (numberB > max)
+{
+    max = numberB;
+}
+if (numberC > max)
+{
+    max = numberC;
+}
+Console.WriteLine(max);
