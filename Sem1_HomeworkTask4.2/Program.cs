f45463ef88b8ba1d3ed41a25313d722a405f5f28
BogdanKorolev <bkorolevjob@gmail.com>
2022-07-12 19:26:09 +0300

    Task4.2 code written

---
+++ Sem1_HomeworkTask4.2/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 4.2: Напишите программу,
+которая принимает на вход три числа
+и выдаёт максимальное из этих чисел: 22 3 9 -> 22 */
+
+int numberA = 22;
+int numberB = 3;
+int numberC = 9;
+int max = numberA;
+if (numberB > max)
+{
+    max = numberB;
+}
+if (numberC > max)
+{
+    max = numberC;
+}
+Console.WriteLine(max);
