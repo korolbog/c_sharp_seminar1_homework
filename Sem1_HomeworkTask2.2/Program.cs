b76eb8f86556bffbbe2c4513e0f8f8edb1940e35
BogdanKorolev <bkorolevjob@gmail.com>
2022-07-12 19:11:45 +0300

    Task2.2 code written

---
+++ Sem1_HomeworkTask2.2/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Задача 2.2: Напишите программу,
+которая на вход принимает два числа
+и выдаёт, какое число большее,
+а какое меньшее: a = -9 b = -3 -> max = -3*/
+
+int numberA = -9;
+int numberB = -3;
+int max = numberA;
+if (numberB > max)
+{
+    max = numberB;
+}
+Console.WriteLine(max);
