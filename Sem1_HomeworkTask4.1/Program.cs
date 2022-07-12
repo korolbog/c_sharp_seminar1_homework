a1eb4d5ad30a327e9f75e7cf28764b5d5ad4bb5a
BogdanKorolev <bkorolevjob@gmail.com>
2022-07-12 19:23:12 +0300

    Task4.1 code written

---
+++ Sem1_HomeworkTask4.1/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 4.1: Напишите программу,
+которая принимает на вход три числа
+и выдаёт максимальное из этих чисел: 44 5 78 -> 78 */
+
+int numberA = 44;
+int numberB = 5;
+int numberC = 78;
+int max = numberA;
+if (numberB > max)
+{
+    max = numberB;
+}
+if (numberC > max)
+{
+    max = numberC;
+}
+Console.WriteLine(max);
